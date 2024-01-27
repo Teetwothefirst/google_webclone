353e71c62293021abdc07e14d86e8c12a2ac1d89
Teetwothefirst <dexterdavid835@gmail.com>
2024-01-27 17:54:24 +0100

    Combination of web Projects

---
+++ DavidHerbertChallenge_Dashboard/Readme.md
@@ -0,0 +1,7 @@
+Font-size is 20px for Outliners
+font-size is 16px for the Header
+font-size is 10px for the normal text
+
+#Extra Ideas
+We can add a toggle button the app
+Add Animation to the bell
---
+++ DavidHerbertChallenge_Dashboard/script.js
@@ -0,0 +1,7 @@
+const topPlatformData = [2500000, 1800000, 1200000,1100000]
+
+//For Each of this value split it and assign it as the inner value of an element
+topPlatformData.forEach((data)=>{
+console.log(data);
+    
+})
